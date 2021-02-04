4e8166e8d15c42b005af66aad73a859d81cfc0dd
boyallex <60341259+boyallex@users.noreply.github.com>
2021-02-04 13:40:49 +0300

    Dev

--- src/js/scripts/slider.js
+++ src/js/scripts/slider.js
@@ -1,27 +1,12 @@
 $(document).ready(function () {
     $('.discounts__slider').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: true,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3  
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }
-    ]
+        // adaptiveHeight: true, 
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        centerPadding: '19px',
+        mobileFirst: true,
+        slidesToShow: 1,
 })
 })
