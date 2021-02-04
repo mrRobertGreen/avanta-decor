609467ebf7d9fe90ca79258150915ccf07b83417
boyallex <60341259+boyallex@users.noreply.github.com>
2021-02-04 13:54:46 +0300

    Dev

--- src/js/scripts/slider.js
+++ src/js/scripts/slider.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     $('.discounts__slider').slick({
-        // adaptiveHeight: true, 
+        adaptiveHeight: true, 
         autoplay: true,
         arrows: false,
         dots: true,
         centerMode: true,
-        centerPadding: '19px',
+        centerPadding: '25px',
         mobileFirst: true,
         slidesToShow: 1,
+        focusOnSelect: true,
+        swipe: true,
+        touchMove: true,
+        variableWidth: true
 })
 })
