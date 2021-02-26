a7c4aa556ce98543077462faef93a39e9bd43c2e
Robert Green <mrrobertgreen17@gmail.com>
2021-02-26 15:00:01 +0300

    work

--- src/js/scripts/header.js
+++ src/js/scripts/header.js
@@ -1,6 +1,6 @@
 $(window).scroll(function () {
     // make header fixed on top
-    if (window.matchMedia('(min-width: 900px)').matches) {
+    if (window.matchMedia('(min-width: 1250px)').matches) {
         if ($(this).scrollTop() >= window.innerHeight) {
             $('.header_fake').css("display", "block");
             $('.header').addClass('_fixed');
