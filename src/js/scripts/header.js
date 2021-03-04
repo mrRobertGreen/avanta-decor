5229decc508c6f11ce0a7b1ae9841901f6dc13af
Robert Green <mrrobertgreen17@gmail.com>
2021-03-04 10:32:53 +0300

    work

--- src/js/scripts/header.js
+++ src/js/scripts/header.js
@@ -1,10 +1,41 @@
 $(window).scroll(function () {
     const menuOffset = document.querySelector(".menu").offsetTop + window.innerHeight
-    if ($(window).scrollTop() < window.innerHeight || $(window).scrollTop() > menuOffset) {
-        $('.header').removeClass('_fixed');
-        $('.header_fake').css("display", "none");
+    const headerHeight = getHeaderHeight()
+    const menuHeight = 70
+    const scrollTop = $(window).scrollTop()
+    if (scrollTop < window.innerHeight || scrollTop > (menuOffset - headerHeight)) {
+        // if (scrollTop < menuOffset + headerHeight) {
+            // const difference = scrollTop - menuOffset
+            // console.log(difference);
+            // $(".header").css("top", -difference)
+        // } else {
+            $('.header').removeClass('_fixed');
+            $('.header_fake').css("display", "none");
+            $(".header").css("top", 0)
+        // }
     } else {
         $('.header_fake').css("display", "block");
         $('.header').addClass('_fixed');
     }
 })
+
+const animateHeaderMoving = () => {
+    const currScrollTop = $(window).scrollTop()
+        const difference = currScrollTop - startScrollTop
+        if (difference > getHeaderHeight()) {
+            $('.header').removeClass('_fixed');
+            $('.header_fake').css("display", "none");
+            $(".header").css("top", 0)
+            $(window).off("scroll", animateHeaderMoving)
+        } else {
+            $(".header").css("top", -difference)
+        }
+}
+
+const getHeaderHeight = () => {
+    if (window.matchMedia("(min-width: 600px)").matches) {
+        return 122
+    } else {
+        return 85
+    }
+}
